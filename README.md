c86beb659b0a723ee640bb01fa64a750b12d3a27
NoahSchiro <noahschiro@rocketmail.com>
2023-10-06 12:01:46 -0400

    Added a proper dataset

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Impressionist GAN in PyTorch
 
 Much of Andy Warhol's work had to do with the industrialization and commercialization of art. He was obsessed with the repetition in art, evident in [Cambell's Soup Cans](https://en.wikipedia.org/wiki/Campbell%27s_Soup_Cans) and the mechanization of his work via [silkscreening](https://en.wikipedia.org/wiki/Screen_printing). In an interview in 1963, Warhol was asked why he painted the same image over and over, he responsed "The reason I'm painting this way is that **I want to be a machine.**"
 
-![](./doityourself.png)
+![](./assets/doityourself.png)
 
 Some of my favorite paintings is a lesser-known series by Warhol, titled *Do It Yourself*. In particular I really enjoy *Do It Yourself (Sailboat)* (depited above). This series of painting was a critique (or perhaps in reverence of) [paint by numbers kits](https://en.wikipedia.org/wiki/Paint_by_number), which were sold so that people could make art at home. While the concept of paint by numbers ties into the commodification of art as a whole, it also brings up questions of orginiality and creativity. What is art if it's as simple as painting by numbers?
 
---
+++ data.py
@@ -0,0 +1,41 @@
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
+from torchvision.transforms import functional as F
+
+
+# I need to create a custom transform
+class ResizeAspectRatio:
+    def __init__(self, targetWidth, targetHeight):
+        self.size = (targetWidth, targetHeight) 
+
+    def __call__(self, img):
+
+        w, h = img.size
+
+        aspect_ratio = float(h) / float(w)
+
+        # If aspect ratio is greater than 1, this means height
+        # is larger than width (i.e. a portrait). So the width
+        # is the lower bound
+        if aspect_ratio > 1:
+
+            # Resize to this
+            img = F.resize(img, self.size[0])
+        else:
+            # Resize to this
+            img = F.resize(img, self.size[1])
+
+        # Crop down from the top left corner
+        return F.crop(img, 0, 0, self.size[1], self.size[0])
+ 
+
+targetw, targeth = 1080, 1080
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5) # TODO: How can we improve these?
+
+transform = T.Compose([
+    ResizeAspectRatio(targetw, targeth),
+    T.ToTensor(),
+    T.Normalize(*stats)
+])
+
+data = ImageFolder("./data", transform)
