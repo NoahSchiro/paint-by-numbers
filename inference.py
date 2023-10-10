8742486531468f6ae1ff45a2d0ec3503408c7c90
NoahSchiro <noahschiro@rocketmail.com>
2023-10-10 11:40:20 -0400

    Added some memory saving measures to inference script

--- inference.py
+++ inference.py
@@ -9,13 +9,14 @@ LATENT_SIZE = 256
 IMAGE_SIZE  = 128
 STATS       = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
 
+@torch.no_grad()
 def save_img(g, id):
 
     # Create a random vector
     static_latent = torch.randn(1, LATENT_SIZE, device=DEVICE)
 
     def denorm(img_tensors):
-            return img_tensors * STATS[1][0] + STATS[0][0]
+        return img_tensors * STATS[1][0] + STATS[0][0]
 
     # Create a painting, denormalize
     img = denorm(g(static_latent))
@@ -24,11 +25,17 @@ def save_img(g, id):
     save_image(img, f"models/inference{id}.png")
 
 if __name__=="__main__":
-    model = Generator(LATENT_SIZE, IMAGE_SIZE).to(DEVICE)
-    model.load_state_dict(torch.load(FILE_PATH, map_location=DEVICE))
 
-    model.eval()
+    torch.cuda.empty_cache()
 
-    # Generate 10 images
-    for i in range(1, 16+1):
-        save_img(model, i)
+    with torch.no_grad():
+        model = Generator(LATENT_SIZE, IMAGE_SIZE).to(DEVICE)
+        model.load_state_dict(torch.load(FILE_PATH, map_location=DEVICE))
+
+        model.eval()
+        print("Model loaded")
+
+        # Generate 10 images
+        for i in range(1, 15+1):
+            print(f"Generating image {i}")
+            save_img(model, i)
